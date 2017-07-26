25a5b62c9d256e80512dae06ca232e97e7e24fc2
James Till Matta <jamesmatta@gmail.com>
2017-07-26 16:05:37 -0400

    Added a names and flat circles
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- libpd/flat_sources.py
+++ libpd/flat_sources.py
@@ -2,32 +2,34 @@
 squares to be made"""
 
 from libpd.geom_base import Shape
+import numpy as np
 
 
 class Square(Shape):
     """This class allows the creation of a flat patch, with some arbitrary
     center and rotation"""
-    def __init__(self, center, edge_vec1, edge_vec2, rotation):
+    def __init__(self, name, center, basis_vecs, rotation):
         """This creates a square, flat, patch
 
         Parameters
         ----------
+        name : str
+            The name for this source
         center : vector
             center point of the square patch
-        edge_vec1 : vector
-            vector from center to the middle of one of the edges
-        edge_vec2 : vector
-            vector from center to the middle of another edge (must be
-            orthogonal to edge_vec1)
+        basis_vecs : tuple of vectors
+            tuple of two orthogonal vectors from the center to the middle of
+            edges
         rotation : geom_base.Rotation
             The rotation matrix that moves the patch into the correct
             orientation
         """
+        self.name = name
         self.center = center
         # for this shape there is no need to store the transformation, simply
         # apply it to the two basis vectors and then ignore it
-        self.vec1 = rotation.rmat.dot(edge_vec1)
-        self.vec2 = rotation.rmat.dot(edge_vec2)
+        self.vec1 = rotation.rmat.dot(basis_vecs[0])
+        self.vec2 = rotation.rmat.dot(basis_vecs[1])
         self.bounds = [(-1.0, 1.0), (-1.0, 1.0)]
 
     def get_num_integral_params(self):
@@ -54,7 +56,7 @@ class Square(Shape):
         """
         return self.bounds
 
-    def get_position(self, scale1, scale2):
+    def get_position(self, *args):
         """Given a set of parameter values (in the same order as the bounds
         array) this returns the x,y,z position corresponding to those bounds
 
@@ -70,4 +72,367 @@ class Square(Shape):
         position : vector
             The position corresponding to those integration parameters
         """
-        return self.center + scale1*self.vec1 + scale2*self.vec2
+        return self.center + args[0]*self.vec1 + args[1]*self.vec2
+
+    def __str__(self):
+        """Returns the string representation of the object
+
+        Returns
+        -------
+        out_str : str
+            The string representation of the source
+        """
+        out_strs = []
+        out_strs.append("Square Patch - {0:s}\n".format(self.name))
+        temp = "Centered At: ({0:4.2f}, {1:4.2f}, {2:4.2f})\n"
+        out_strs.append(temp.format(self.center[0], self.center[1],
+                                    self.center[2]))
+        temp = "First Basis Vector: ({0:4.2f}, {1:4.2f}, {2:4.2f})\n"
+        out_strs.append(temp.format(self.vec1[0], self.vec1[1], self.vec1[2]))
+        temp = "Second Basis Vector: ({0:4.2f}, {1:4.2f}, {2:4.2f})"
+        out_strs.append(temp.format(self.vec2[0], self.vec2[1], self.vec2[2]))
+        return "".join(out_strs)
+
+    __repr__ = __str__
+
+
+class Circle(Shape):
+    """This class allows the creation of a flat circle, with some arbitrary
+    center and rotation"""
+    def __init__(self, name, center, radius, rotation):
+        """This creates a flat circle, with arbitrary rotation
+
+        Parameters
+        ----------
+        name : str
+            The name for this source
+        center : vector
+            center point of the square patch
+        radius : float
+            radius of the circle
+        rotation : geom_base.Rotation
+            The rotation matrix that moves the patch into the correct
+            orientation
+        """
+        self.name = name
+        self.center = center
+        # for this shape there is no need to store the transformation, simply
+        # apply it to the two basis vectors and then ignore it
+        self.rad = radius
+        self.rmat = rotation.rmat
+        self.bounds = [(0.0, 2.0*np.pi), (0.0, 1.0)]
+
+    def get_num_integral_params(self):
+        """Returns the number of parameters that will need to be integrated
+        over
+
+        Returns
+        -------
+        num_params : int
+            the number of parameters that will need to be integrated over to
+            integrate over the volume of the object
+        """
+        return len(self.bounds)
+
+    def get_integral_bounds(self):
+        """Returns the bounds of each of the parameters to be integrated over
+
+        Returns
+        -------
+        bounds : list of lists of tuples
+            for each parameter to be integrated across, each sublist contains a
+            tuple with the upper and lower bounds of the parameter, each sub-
+            list is one set of bounds
+        """
+        return self.bounds
+
+    def get_position(self, *args):
+        """Given a set of parameter values (in the same order as the bounds
+        array) this returns the x,y,z position corresponding to those bounds
+
+        Parameters
+        ----------
+        theta : float
+            The angle of the radius vector
+        scale2 : float
+            The scaling of the radius vector
+
+        Returns
+        -------
+        position : vector
+            The position corresponding to those integration parameters
+        """
+        rvec = self.rmat.dot((self.rad * np.array([np.cos(args[0]),
+                                                   np.sin(args[0]), 0.0])))
+        return self.center + args[1]*rvec
+
+    def __str__(self):
+        """Returns the string representation of the object
+
+        Returns
+        -------
+        out_str : str
+            The string representation of the source
+        """
+        out_strs = []
+        out_strs.append("Circular Patch - {0:s}\n".format(self.name))
+        temp = "Centered At: ({0:4.2f}, {1:4.2f}, {2:4.2f})\n"
+        out_strs.append(temp.format(self.center[0], self.center[1],
+                                    self.center[2]))
+        out_strs.append("Radius: {0:4.2f}\n".format(self.rad))
+        out_strs.append("Rotation Matrix:\n{0:s}".format(self.rmat.__str__()))
+        return "".join(out_strs)
+
+    __repr__ = __str__
+
+
+class CircleXY(Shape):
+    """This class allows the creation of a flat circle parallel to the XY
+    plane, with some arbitrary center and *no* rotation"""
+    def __init__(self, name, center, radius):
+        """This creates a flat circle, with arbitrary rotation
+
+        Parameters
+        ----------
+        name : str
+            The name for this source
+        center : vector
+            center point of the square patch
+        radius : float
+            radius of the circle
+        """
+        self.name = name
+        self.center = center
+        # for this shape there is no need to store the transformation, simply
+        # apply it to the two basis vectors and then ignore it
+        self.rad = radius
+        self.bounds = [(0.0, 2.0*np.pi), (0.0, 1.0)]
+
+    def get_num_integral_params(self):
+        """Returns the number of parameters that will need to be integrated
+        over
+
+        Returns
+        -------
+        num_params : int
+            the number of parameters that will need to be integrated over to
+            integrate over the volume of the object
+        """
+        return len(self.bounds)
+
+    def get_integral_bounds(self):
+        """Returns the bounds of each of the parameters to be integrated over
+
+        Returns
+        -------
+        bounds : list of lists of tuples
+            for each parameter to be integrated across, each sublist contains a
+            tuple with the upper and lower bounds of the parameter, each sub-
+            list is one set of bounds
+        """
+        return self.bounds
+
+    def get_position(self, *args):
+        """Given a set of parameter values (in the same order as the bounds
+        array) this returns the x,y,z position corresponding to those bounds
+
+        Parameters
+        ----------
+        theta : float
+            The angle of the radius vector
+        scale2 : float
+            The scaling of the radius vector
+
+        Returns
+        -------
+        position : vector
+            The position corresponding to those integration parameters
+        """
+        rvec = self.rad * np.array([np.cos(args[0]), np.sin(args[0]), 0.0])
+        return self.center + args[1]*rvec
+
+    def __str__(self):
+        """Returns the string representation of the object
+
+        Returns
+        -------
+        out_str : str
+            The string representation of the source
+        """
+        out_strs = []
+        out_strs.append("XY Circular Patch - {0:s}\n".format(self.name))
+        temp = "Centered At: ({0:4.2f}, {1:4.2f}, {2:4.2f})\n"
+        out_strs.append(temp.format(self.center[0], self.center[1],
+                                    self.center[2]))
+        out_strs.append("Radius: {0:4.2f}".format(self.rad))
+        return "".join(out_strs)
+
+    __repr__ = __str__
+
+
+class CircleXZ(Shape):
+    """This class allows the creation of a flat circle parallel to the XZ
+    plane, with some arbitrary center and *no* rotation"""
+    def __init__(self, name, center, radius):
+        """This creates a flat circle, with arbitrary rotation
+
+        Parameters
+        ----------
+        name : str
+            The name for this source
+        center : vector
+            center point of the square patch
+        radius : float
+            radius of the circle
+        """
+        self.name = name
+        self.center = center
+        # for this shape there is no need to store the transformation, simply
+        # apply it to the two basis vectors and then ignore it
+        self.rad = radius
+        self.bounds = [(0.0, 2.0*np.pi), (0.0, 1.0)]
+
+    def get_num_integral_params(self):
+        """Returns the number of parameters that will need to be integrated
+        over
+
+        Returns
+        -------
+        num_params : int
+            the number of parameters that will need to be integrated over to
+            integrate over the volume of the object
+        """
+        return len(self.bounds)
+
+    def get_integral_bounds(self):
+        """Returns the bounds of each of the parameters to be integrated over
+
+        Returns
+        -------
+        bounds : list of lists of tuples
+            for each parameter to be integrated across, each sublist contains a
+            tuple with the upper and lower bounds of the parameter, each sub-
+            list is one set of bounds
+        """
+        return self.bounds
+
+    def get_position(self, *args):
+        """Given a set of parameter values (in the same order as the bounds
+        array) this returns the x,y,z position corresponding to those bounds
+
+        Parameters
+        ----------
+        theta : float
+            The angle of the radius vector
+        scale2 : float
+            The scaling of the radius vector
+
+        Returns
+        -------
+        position : vector
+            The position corresponding to those integration parameters
+        """
+        rvec = self.rad * np.array([np.cos(args[0]), 0.0, np.sin(args[0])])
+        return self.center + args[1]*rvec
+
+    def __str__(self):
+        """Returns the string representation of the object
+
+        Returns
+        -------
+        out_str : str
+            The string representation of the source
+        """
+        out_strs = []
+        out_strs.append("XZ Circular Patch - {0:s}\n".format(self.name))
+        temp = "Centered At: ({0:4.2f}, {1:4.2f}, {2:4.2f})\n"
+        out_strs.append(temp.format(self.center[0], self.center[1],
+                                    self.center[2]))
+        out_strs.append("Radius: {0:4.2f}".format(self.rad))
+        return "".join(out_strs)
+
+    __repr__ = __str__
+
+
+class CircleYZ(Shape):
+    """This class allows the creation of a flat circle parallel to the YZ
+    plane, with some arbitrary center and *no* rotation"""
+    def __init__(self, name, center, radius):
+        """This creates a flat circle, with arbitrary rotation
+
+        Parameters
+        ----------
+        name : str
+            The name for this source
+        center : vector
+            center point of the square patch
+        radius : float
+            radius of the circle
+        """
+        self.name = name
+        self.center = center
+        # for this shape there is no need to store the transformation, simply
+        # apply it to the two basis vectors and then ignore it
+        self.rad = radius
+        self.bounds = [(0.0, 2.0*np.pi), (0.0, 1.0)]
+
+    def get_num_integral_params(self):
+        """Returns the number of parameters that will need to be integrated
+        over
+
+        Returns
+        -------
+        num_params : int
+            the number of parameters that will need to be integrated over to
+            integrate over the volume of the object
+        """
+        return len(self.bounds)
+
+    def get_integral_bounds(self):
+        """Returns the bounds of each of the parameters to be integrated over
+
+        Returns
+        -------
+        bounds : list of lists of tuples
+            for each parameter to be integrated across, each sublist contains a
+            tuple with the upper and lower bounds of the parameter, each sub-
+            list is one set of bounds
+        """
+        return self.bounds
+
+    def get_position(self, *args):
+        """Given a set of parameter values (in the same order as the bounds
+        array) this returns the x,y,z position corresponding to those bounds
+
+        Parameters
+        ----------
+        theta : float
+            The angle of the radius vector
+        scale2 : float
+            The scaling of the radius vector
+
+        Returns
+        -------
+        position : vector
+            The position corresponding to those integration parameters
+        """
+        rvec = self.rad * np.array([0.0, np.cos(args[0]), np.sin(args[0])])
+        return self.center + args[1]*rvec
+
+    def __str__(self):
+        """Returns the string representation of the object
+
+        Returns
+        -------
+        out_str : str
+            The string representation of the source
+        """
+        out_strs = []
+        out_strs.append("YZ Circular Patch - {0:s}\n".format(self.name))
+        temp = "Centered At: ({0:4.2f}, {1:4.2f}, {2:4.2f})\n"
+        out_strs.append(temp.format(self.center[0], self.center[1],
+                                    self.center[2]))
+        out_strs.append("Radius: {0:4.2f}".format(self.rad))
+        return "".join(out_strs)
+
+    __repr__ = __str__
