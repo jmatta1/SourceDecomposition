661615937a320be2ca83b0b79462e8833c9ee135
James Till Matta <jamesmatta@gmail.com>
2017-07-26 10:18:12 -0400

    Fixed import structure a bit
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- libpd/__init__.py
+++ libpd/__init__.py
@@ -1,6 +1,3 @@
 """This is the init file for the position decomp library"""
 import libpd.detector as detector
-# import libpositiondecomp.low_dim_sources as low_dim_sources
-# import libpositiondecomp.square_patch as square_patch
-# import libpositiondecomp.circular_patch as circular_patch
-# import libpositiondecomp.cylinder as cylinder
+import libpd.flat_sources as flat_sources
