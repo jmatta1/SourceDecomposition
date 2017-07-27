49d6441417df1f0d10a92896988a1aaee4606290
James Till Matta <jamesmatta@gmail.com>
2017-07-26 20:09:04 -0400

    Added wrappers for current backend functions
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ libpd/cpp/wrappers.cpp
@@ -0,0 +1,60 @@
+#include"wrappers.h"
+#include"detector.h"
+#include"flat_shapes.h"
+#include"calculator.h"
+
+void* makeDetector(double* vec1, double* vec2, double* norm)
+{
+    Detector* temp = new Detector(vec1, vec2, norm);
+    return (void*)temp;
+}
+
+// Make 2D source shapes
+void* makeSquare(double* cent, double* vec1, double* vec2)
+{
+    Square* temp = new Square(cent, vec1, vec2);
+    return (void*)temp;
+}
+
+void* makeCircle(double* cent, double radius, double* rot)
+{
+    Circle* temp = new Circle(cent, radius, rot);
+    return (void*)temp;
+}
+
+void* makeCircleXY(double* cent, double radius);
+{
+    CircleXY* temp = new CircleXY(cent, radius);
+    return (void*)temp;
+}
+
+void* makeCircleXZ(double* cent, double radius)
+{
+    CircleXZ* temp = new CircleXZ(cent, radius);
+    return (void*)temp;
+}
+
+void* makeCircleYZ(double* cent, double radius);
+{
+    CircleYZ* temp = new CircleYZ(cent, radius);
+    return (void*)temp;
+}
+
+// Function to create the calculator object
+void* makeCalculator(void* detector, void* source)
+{
+    Calculator* temp = new Calculator((Detector*)detector, (Shape*)source);
+    return (void*)temp;
+}
+
+void freeCalculator(void* calcObject)
+{
+    Calculator* temp = (Calculator*)calcObject;
+    delete temp;
+}
+
+// Function to perform the calculation
+double calculateIntegrand(int n, double* params, void* calcObject)
+{
+    return ((Calculator*)calcObject)->calcIntegrand(params);
+}
--- libpd/cpp/wrappers.h
+++ libpd/cpp/wrappers.h
@@ -1,6 +1,22 @@
 #ifndef POSITION_DECOMP_LIBPD_CPP_WRAPPERS_H
 #define POSITION_DECOMP_LIBPD_CPP_WRAPPERS_H
 
+// Make detector shape
+extern "C" void* makeDetector(double* vec1, double* vec2, double* norm);
 
+// Make source shapes
+extern "C" void* makeSquare(double* cent, double* vec1, double* vec2);
+extern "C" void* makeCircle(double* cent, double radius, double* rot);
+extern "C" void* makeCircleXY(double* cent, double radius);
+extern "C" void* makeCircleXZ(double* cent, double radius);
+extern "C" void* makeCircleYZ(double* cent, double radius);
+
+// Function to create and destroy the calculator object
+// the calculator object destroys the source and detector objects that it owns
+extern "C" void* makeCalculator(void* detector, void* source);
+extern "C" void freeCalculator(void* calcObject);
+
+// Function to perform the calculation
+extern "C" double calculateIntegrand(int n, double* params, void* calcObject);
 
 #endif //POSITION_DECOMP_LIBPD_CPP_WRAPPERS_H
