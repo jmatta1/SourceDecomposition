ef33bd42bc0d36ed99e35dc92154f4680875cbea
James Till Matta <jamesmatta@gmail.com>
2017-07-28 13:49:32 -0400

    Add shell shapes to module
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- libpd/__init__.py
+++ libpd/__init__.py
@@ -1,4 +1,5 @@
 """This is the init file for the position decomp library"""
 import libpd.detector as detector
 import libpd.flat_sources as flat_sources
+import libpd.shell_sources as shell_sources
 import libpd.weight_calc as weight_calc
