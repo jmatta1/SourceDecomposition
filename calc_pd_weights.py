affac0151d5588b0679a6cca521f10c445702116
James Till Matta <jamesmatta@gmail.com>
2017-08-03 17:07:00 -0400

    Update how main is called
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- calc_pd_weights.py
+++ calc_pd_weights.py
@@ -7,25 +7,23 @@ from libpd import detector as dt
 from libpd import source_construction as sc
 import libpd.weight_calc as wc
 
-def main():
+def main(nai_pos_path, core_count, out_name):
     """Primary entrypoint for the weight calculation code
 
     Parameters
     ----------
-    sys.argv[1] : str
-        path to the list of NaI center points
-    sys.argv[2] : str
+    nai_pos_path : str
+        path to the list of AD1 patches, normals, and sizes
+    core_count : str
         number of cores to use for the calculation
+    out_name : str
+        name of the output file
     """
-    if len(sys.argv) != 4:
-        print USAGE.format(sys.argv[0])
-        sys.exit()
-    detectors = dt.make_nai_list(dt.read_positions(sys.argv[1]))
-    num_cores = int(sys.argv[2])
+    detectors = dt.make_nai_list(dt.read_positions(nai_pos_path)
     sources = sc.set_up_no_wall_sources()
     sources.extend(sc.make_cube_corner_sources())
-    weights = wc.calculate_weights(detectors, sources, num_cores)
-    out_file = open(sys.argv[3], 'w')
+    weights = wc.calculate_weights(detectors, sources, core_count)
+    out_file = open(out_name, 'w')
     fmt_str = "{0:d}, {1:d}, {2:s}, {3:10.8e}\n"
     for elem in weights:
         out_file.write(fmt_str.format(*elem))
@@ -37,4 +35,7 @@ USAGE = """Usage:
 """
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 4:
+        print USAGE.format(sys.argv[0])
+        sys.exit()
+    main(sys.argv[1], int(sys.argv[2]), sys.argv[3])
--- calc_ss_weights.py
+++ calc_ss_weights.py
@@ -8,27 +8,23 @@ from libpd import detector as dt
 from libpd import source_construction as sc
 import libpd.weight_calc as wc
 
-def main():
+def main(patch_info_path, core_count, out_name):
     """Primary entrypoint for the weight calculation code
 
     Parameters
     ----------
-    sys.argv[1] : str
+    patch_info_path : str
         path to the list of AD1 patches, normals, and sizes
-    sys.argv[2] : str
+    core_count : str
         number of cores to use for the calculation
-    sys.argv[3] : str
+    out_name : str
         name of the output file
     """
-    if len(sys.argv) != 4:
-        print USAGE.format(sys.argv[0])
-        sys.exit()
-    detectors = dt.read_patches(sys.argv[1])
-    num_cores = int(sys.argv[2])
+    detectors = dt.read_patches(patch_info_path)
     sources = sc.set_up_no_wall_sources()
     sources.extend(sc.make_cube_corner_sources())
-    weights = wc.calculate_weights(detectors, sources, num_cores)
-    out_file = open(sys.argv[3], 'w')
+    weights = wc.calculate_weights(detectors, sources, core_count)
+    out_file = open(out_name, 'w')
     fmt_str = "{0:d}, {1:d}, {2:s}, {3:10.8e}\n"
     for elem in weights:
         out_file.write(fmt_str.format(*elem))
@@ -36,8 +32,11 @@ def main():
 
 
 USAGE = """Usage:
-    {0:s} <Path To NaI Center Points File> <Number of Cores> <Ouput File Name>
+    {0:s} <Path To Patch Info File> <Number of Cores> <Ouput File Name>
 """
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 4:
+        print USAGE.format(sys.argv[0])
+        sys.exit()
+    main(sys.argv[1], int(sys.argv[2]), sys.argv[3])
