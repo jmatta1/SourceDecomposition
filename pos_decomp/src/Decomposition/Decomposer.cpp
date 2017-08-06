f73a6b333558163861b969ecd169401a78961b12
James Till Matta <jamesmatta@gmail.com>
2017-08-06 19:38:35 -0400

    Implemented the decomposition algorithms
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ pos_decomp/src/Decomposition/Decomposer.cpp
@@ -0,0 +1,119 @@
+#include"Decomposer.h"
+#include<iostream>
+#include<cmath>
+
+Decomposer::Decomposer(ResMatData& respDat, ScanData& scanDat)
+{
+    std::tie(respMat, respMatTr, respMatProj, numPositions, numSources) = respDat;
+    int placeholder = 0;
+    std::tie(scanData, numEnergyBins, placeholder) = scanDat;
+    
+    newVals = new double[numSources];
+    oldVals = new double[numSources];
+    products = new double[numSources];
+    for(int i=0; i<numSources; ++i)
+    {
+        newVals[i] = 0.0;
+        oldVals[i] = 0.0;
+        products[i] = 0.0;
+    }
+    
+    mults = new double[numPositions];
+    for(int i=0; i<numPositions; ++i)
+    {
+        mults[i] = 0.0;
+    }
+    
+    placeholder = numSources*numEnergyBins;
+    outputArray = new double[placeholder];
+    for(int i=0; i<placeholder; ++i)
+    {
+        outputArray[i] = 1.0;
+    }
+}
+
+void Decomposer::performDecomp()
+{
+    // to do the serial decomposition we need to iterate over the data bins
+    for(int i=0; i<numEnergyBins; ++i)
+    {
+        if((i%Internal::DisplayChunk)==0) std::cout<<"Position decomp on data bin: "<<i+1<<"/"<<numEnergyBins;
+        int temp = decomposeSingleBin(i);
+        if((i%Internal::DisplayChunk)==0) std::cout<<"  - Conveged at iteration: "<<temp<<std::endl;
+    }
+}
+
+int Decomposer::decomposeSingleBin(int index)
+{
+    double* data = (scanData+index*numPositions);
+    for(int i=0; i<numSources; ++i)
+    {
+        newVals[i] = 1.0e-10;
+        oldVals[i] = outputArray[index*numSources + i];
+    }
+    
+    unsigned long long i = 0;
+    for(; !hasConverged; ++i)
+    {
+        //calculate multipliers
+        for(int j=0; j<numPositions; ++j)
+        {
+            double denom = 0.0;
+            int offset = j*numSources;
+            for(int k=0; k<numSources; k+=2)
+            {
+                denom += oldVals[k]*respMatTr[offset+k];
+                denom += oldVals[k+1]*respMatTr[offset+k+1];
+            }
+            mults[j] = data[j]/denom;
+        }
+        
+        //calculate vector resp function multipliers
+        for(int j=0; j<numSources; ++j)
+        {
+            products[j] = 0.0;
+            int offset = j*numPositions;
+            for(int k=0; k<numPositions; k+=2)
+            {
+                products[j] += respMat[offset+k]*mults[k];
+                products[j] += respMat[offset+k+1]*mults[k+1];
+            }
+        }
+        
+        //calculate the new values
+        for(int j=0; j<numSources; j+=2)
+        {
+            newVals[j] = (products[j]*oldVals[j])/respMatProj[j];
+            newVals[j+1] = (products[j+1]*oldVals[j+1])/respMatProj[j+1];
+        }
+        
+        //check convergence
+        hasConverged = checkConvergence();
+        //swap new values into old values and vice versa
+        double* temp = oldVals;
+        oldVals = newVals;
+        newVals = temp;
+    }
+    
+    for(int i=0; i<numSources; ++i)
+    {
+        outputArray[index*numSources + i] = oldVals[i];
+    }
+}
+
+bool Decomposer::checkConvergence()
+{
+    for(int i=0; i<numSources; ++i)
+    {
+        if(newVals[i] < Internal::ValueThreshold && oldVals[i] < Internal::ValueThreshold)
+        {
+            continue;
+        }
+        double frac = std::abs((newVals[i]-oldVals[i])/((oldVals[i]+newVals[i])/2.0));
+        if(frac > Internal::ConvLimit)
+        {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ pos_decomp/src/Decomposition/Decomposer.h
@@ -0,0 +1,46 @@
+#ifndef POSITIONDECOMPOSER_SRC_DECOMPOSITION_DECOMPOSER_H
+#define POSITIONDECOMPOSER_SRC_DECOMPOSITION_DECOMPOSER_H
+#include"RootInterface/ScanGrabber.h"
+#include"RootInterface/ResponseMatrixBuilder.h"
+
+namespace Internal
+{
+static const int DisplayChunk = 50;
+
+static const double ValueThreshold = 1.0e-9;
+
+static const double ConvLimit = 0.005;
+}
+
+class Decomposer
+{
+public:
+    Decomposer(ResMatData& respDat, ScanData& scanDat);
+    ~Decomposer(){delete[] newVals; delete[] oldVals; delete[] mults; delete[] products; delete[] outputArray;}
+
+    void performDecomp();
+    double* getFinalResult(){if(!hasConverged) performDecomp(); return outputArray;}
+
+private:
+    int decomposeSingleBin(int index);
+    bool checkConvergence();
+    //data we do not own, we merely have pointers to it
+    double* respMat = nullptr; // the position response matrix
+    double* respMatTr = nullptr; // the transpose of the position response matrix
+    double* respMatProj = nullptr; // the projection of the position response matrix onto its Y-Axis
+    double* scanData = nullptr; // the scan data arranged as each row is numPositions long and corresponds to a single energy bin at every position
+    
+    bool hasConverged = false;
+    
+    //owned data, so we need to delete it on shutdown
+    double* newVals = nullptr;
+    double* oldVals = nullptr;
+    double* mults = nullptr;
+    double* products = nullptr;
+    double* outputArray = nullptr;
+    int numSources;
+    int numPositions;
+    int numEnergyBins;
+};
+
+#endif //POSITIONDECOMPOSER_SRC_DECOMPOSITION_DECOMPOSER_H
