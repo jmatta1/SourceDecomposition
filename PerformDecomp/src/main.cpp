4a71ad487f7b58135580490247cfcbecd4bb685f
James Till Matta <jamesmatta@gmail.com>
2017-08-08 09:55:57 -0400

    Added using the decomposition
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- PerformDecomp/src/main.cpp
+++ PerformDecomp/src/main.cpp
@@ -5,7 +5,7 @@
 #include"RootInterface/ScanGrabber.h"
 #include"RootInterface/ResponseMatrixBuilder.h"
 #include"Decomposition/Decomposer.h"
-
+#include"RootInterface/DataDumper.h"
 
 int main(int argc, char* argv[])
 {
@@ -23,11 +23,23 @@ int main(int argc, char* argv[])
     {
         return 1;
     }
+    std::cout << "Reading scan data" << std::endl;
     //use that input to set up the calculation
     ScanGrabber scanData(inputData.scanDataFileName, inputData.numPositions,
                          inputData.numEnergyBins);
+    std::cout << "Building response matrix" << std::endl;
     ResponseMatrixBuilder respMat(inputData.functionList,
                                   inputData.respFuncFileName,
                                   inputData.numPositions);
+    std::cout << "Performaing decomposition" << std::endl;
+    Decomposer calc(respMat.getMatrixInfo(), scanData.getScanData());
+    calc.performDecomp();
+    //dump the data out to the specified root file
+    std::cout << "Dumping results to: " << inputData.outputFileName << std::endl;
+    DataDumper dumper(inputData.outputFileName, inputData.directoryName,
+                      respMat.getMatrixDump(), scanData.getFullScanData(),
+                      calc.getFinalResult(), inputData.functionList);
+    dumper.writeOutput();
+    std::cout << "Done!" << std::endl;
     return 0;
 }
