0bd56e66710e23b809145b3fa57a9067bd40238d
James Till Matta <jamesmatta@gmail.com>
2017-08-30 11:56:44 -0400

    Add empty file for segment manager classes
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

---
+++ RecursiveSegmentation/librecseg/__init__.py
@@ -0,0 +1,5 @@
+"""Initialization file for the librecseg, which contains various components
+for the recursive segmentation optimization code"""
+
+import librecseg.segment_manager as segment_manager
+
---
+++ RecursiveSegmentation/librecseg/segment_manager.py
@@ -0,0 +1,2 @@
+"""This file contains the classes that manage segments and their sub segments
+"""
