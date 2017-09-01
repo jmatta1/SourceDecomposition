68d57afa936cf9596107138f6eefd1c9ace88883
James Till Matta <jamesmatta@gmail.com>
2017-09-01 13:00:11 -0400

    Include utility and trees
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- RecursiveSegmentation/librecseg/__init__.py
+++ RecursiveSegmentation/librecseg/__init__.py
@@ -1,5 +1,6 @@
 """Initialization file for the librecseg, which contains various components
 for the recursive segmentation optimization code"""
 
-import librecseg.segment_manager as segment_manager
+import librecseg.segment_tree as segment_tree
+import librecseg.utility as utility
 
