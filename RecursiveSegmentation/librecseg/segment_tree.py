0c616fc9f73111a430a4faf45a9c57fa268f3220
James Till Matta <jamesmatta@gmail.com>
2017-09-01 15:19:14 -0400

    Added code to handle adjacency checks
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- RecursiveSegmentation/librecseg/segment_tree.py
+++ RecursiveSegmentation/librecseg/segment_tree.py
@@ -32,8 +32,8 @@ class SegmentTree(object):
         for i in range(divs):
             for j in range(divs):
                 name = fmt_str.format(divs, i, j)
-                node = SegmentTreeNode(name)
-                info = SegmentInfo(True, True, node)
+                node = SegmentTreeNode(name, True, (divs, i, j))
+                info = SegmentInfo(True, node)
                 self.seg_dict[name] = info
         # Now, for each level above the leaves make the quartet summed segments
         curr_segs = divs/2
@@ -43,9 +43,11 @@ class SegmentTree(object):
                     name = fmt_str.format(curr_segs, i, j)
                     comp = [self.seg_dict[cname].get_node() for cname in
                             gen_quartet_names(fmt_str, curr_segs, (i, j))]
-                    node = SegmentTreeNode(name)
+                    node = SegmentTreeNode(name, True, (curr_segs, i, j))
                     node.add_composition(comp)
-                    info = SegmentInfo(True, True, node)
+                    for child_node in comp:
+                        child_node.add_parent(node)
+                    info = SegmentInfo(True, node)
                     self.seg_dict[name] = info
             curr_segs /= 2
         self.head = self.seg_dict[fmt_str.format(1,0,0)].get_node()
@@ -53,3 +55,24 @@ class SegmentTree(object):
     def print_tree(self):
         """Prints the tree structure in some visually reasonable manner"""
         self.head.print_self_and_children("", "")
+
+    def test_adjacency(self, func1, func2):
+        """Takes two function names and tests if they are adjacent to each
+        other
+
+        Parameters
+        ----------
+        func1 : str
+            Name of the first function to be tested
+        func2 : str
+            Name of the second function to be tested
+
+        Returns
+        -------
+        are_adjacent : bool
+            True if the two functions have at least one segment or sub-segment
+            adjacent to eachother, False otherwise
+        """
+        node1 = self.seg_dict[func1].get_node()
+        node2 = self.seg_dict[func2].get_node()
+        return node1.is_adjacent(node2)
