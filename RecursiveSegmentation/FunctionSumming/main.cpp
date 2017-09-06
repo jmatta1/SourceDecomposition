71f4d2647a15692b0d4e26bd1437de05a932017d
James Till Matta <jamesmatta@gmail.com>
2017-09-06 08:36:10 -0400

    Added capability to accept multiple input files
    This should speed things up since it only needs to pay the price of loading
    the root libraries once, and then it can do multiple response function sums
    
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- RecursiveSegmentation/FunctionSumming/main.cpp
+++ RecursiveSegmentation/FunctionSumming/main.cpp
@@ -8,53 +8,56 @@
 int main(int argc, char* argv[])
 {
     //check for proper commandline input
-    if(argc != 2)
+    if(argc < 2)
     {
         std::cout << "Usage: \n    ";
-        std::cout << argv[0] << " <Input File Name>" << std::endl;
+        std::cout << argv[0] << " <Input File Name 1> [<Input File Name 2> [<Input File Name 3> ...]]" << std::endl;
         return 1;
     }
-    //read the input from the file given on the command line
-    std::string inFileName(argv[1]);
-    InData inputData;
-    if(!parseAndPrintInputData(&inputData, inFileName, std::cout))
+    for(int i = 1; i<argc; ++i)
     {
-        return 1;
-    }
-    std::cout<<"Creating files and sum hists"<<std::endl;
-    // open the input file
-    TFile* inFile = new TFile(inputData.inputFileName.c_str());
-    // open the output file
-    TFile* outFile = nullptr;
-    if(inputData.resetFile)
-    {
-        outFile = new TFile(inputData.outputFileName.c_str(), "RECREATE");
-    }
-    else
-    {
-        outFile = new TFile(inputData.outputFileName.c_str(), "UPDATE");
-    }
-    //now open the first response function to clone it
-    TH1D* temp = (TH1D*)inFile->Get(inputData.functionList[0].c_str());
-    outFile->cd();
-    TH1D* sumHist = (TH1D*)temp->Clone();
-    sumHist->SetNameTitle(inputData.sumFunctionName.c_str(), inputData.sumFunctionName.c_str());
-    sumHist->Reset("ICESM");
-    delete temp;
-    //now iterate through the list of response functions to sum them
-    std::cout<<"Summing histograms"<<std::endl;
-    for(auto&& x: inputData.functionList)
-    {
-        temp = (TH1D*)inFile->Get(x.c_str());
-        sumHist->Add(temp, 1.0);
+        //read the input from the file given on the command line
+        std::string inFileName(argv[i]);
+        InData inputData;
+        if(!parseAndPrintInputData(&inputData, inFileName, std::cout))
+        {
+            continue;
+        }
+        std::cout<<"Creating files and sum hists"<<std::endl;
+        // open the input file
+        TFile* inFile = new TFile(inputData.inputFileName.c_str());
+        // open the output file
+        TFile* outFile = nullptr;
+        if(inputData.resetFile)
+        {
+            outFile = new TFile(inputData.outputFileName.c_str(), "RECREATE");
+        }
+        else
+        {
+            outFile = new TFile(inputData.outputFileName.c_str(), "UPDATE");
+        }
+        //now open the first response function to clone it
+        TH1D* temp = (TH1D*)inFile->Get(inputData.functionList[0].c_str());
+        outFile->cd();
+        TH1D* sumHist = (TH1D*)temp->Clone();
+        sumHist->SetNameTitle(inputData.sumFunctionName.c_str(), inputData.sumFunctionName.c_str());
+        sumHist->Reset("ICESM");
         delete temp;
+        //now iterate through the list of response functions to sum them
+        std::cout<<"Summing histograms"<<std::endl;
+        for(auto&& x: inputData.functionList)
+        {
+            temp = (TH1D*)inFile->Get(x.c_str());
+            sumHist->Add(temp, 1.0);
+            delete temp;
+        }
+        delete inFile;
+        outFile->cd();
+        sumHist->Write();
+        outFile->Flush();
+        delete sumHist;
+        delete outFile;
+        std::cout << "Done!" << std::endl;
     }
-    delete inFile;
-    outFile->cd();
-    sumHist->Write();
-    outFile->Flush();
-    delete sumHist;
-    delete outFile;
-    std::cout << "Done!" << std::endl;
     return 0;
 }
